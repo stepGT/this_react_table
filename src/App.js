2aa76d78b33217cd0ff6633150034953b4fd323d
Andrey <stepgt@mail.ru>
2021-09-21 14:56:42 +1000

    Refactor array data

--- src/App.js
+++ src/App.js
@@ -9,55 +9,42 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-/**
- * 
- * @param {*} book 
- * @param {*} author 
- * @param {*} language 
- * @param {*} publisher 
- * @param {*} price 
- * @returns 
- */
-function createData(book, author, language, publisher, price) {
-  return { book, author, language, publisher, price };
-}
-
-const rows = [
-  createData(
+const data = [
+  [
     "The Road to React",
     "Robin Wieruch",
     "English",
     "Independently published (September 14, 2018)",
-    "$28.49"
-  ),
-  createData(
+    "$28.49",
+  ],
+  [
     "React and React Native",
     "Adam Boduch",
     "English",
     "Packt Publishing (April 30, 2020)",
-    "$33.24"
-  ),
-  createData(
+    "$33.24",
+  ],
+  [
     "REACT.JS Programming in 8 Hours",
     "Ray Yao",
     "English",
     "Independently published (August 8, 2021)",
-    "$13.99"
-  ),
-  createData(
+    "$13.99",
+  ],
+  [
     "Integrating D3.js with React",
     "Elad Elrom",
     "English",
     "Apress; 1st ed. edition (June 4, 2021)",
-    "$43.19"
-  ),
-  createData(
+    "$43.19",
+  ],
+  [
     "Learn React Hooks",
     "Daniel Bugl",
     "English",
     "Packt Publishing; 1st edition (October 18, 2019)",
-    "$20.63"
-  ),
+    "$20.63",
+  ],
 ];
 
 const headers = [
@@ -70,7 +57,7 @@ const headers = [
 
 function App() {
   const [state, setState] = useState({
-    data: rows,
+    data: data,
     sortby: null,
     descending: false,
     edit: null, // [row index, cell index],
